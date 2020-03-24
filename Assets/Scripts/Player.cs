330c798eda458a67cfe13bf4224039d376aca3d3
datrickpover <48770489+datrickpover@users.noreply.github.com>
2020-03-24 16:12:17 -0400

    Added basic collision

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float runSpeed = 5.0f;
+
+    private RigidBody myRigidBody = null;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<RigidBody2d>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+    }
+
+    private void Run()
+    {
+        if (Input.GetAxis("Horizontal") > 1)
+        {
+            myRigidBody.velocity = runSpeed;
+        }
+
+        if (Input.GetAxis("Horizontal") < 1)
+        {
+            myRigidBody.velocity = runSpeed;
+        }
+    }
+}
