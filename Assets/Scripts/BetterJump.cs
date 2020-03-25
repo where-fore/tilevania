370dab6653c07b75e70d5560e3452f784276890b
datrickpover <48770489+datrickpover@users.noreply.github.com>
2020-03-25 19:39:34 -0400

    BetterBetterBetterBetterJump
    no sticky walls, no double jumps, all in one script

--- Assets/Scripts/BetterJump.cs
+++ Assets/Scripts/BetterJump.cs
@@ -6,6 +6,9 @@ public class BetterJump : MonoBehaviour
 {
     // Config
     [SerializeField]
+    private float jumpSpeed = 9.0f;
+
+    [SerializeField]
     private float fallMultiplier = 3.5f;
 
     [SerializeField]
@@ -14,19 +17,55 @@ public class BetterJump : MonoBehaviour
     [SerializeField]
     private float gravityMultiplier = 4f;
 
+    [SerializeField]
+    private string layerToTouchForRefreshedJumpString = "Ground";
+    private int layerToTouchForRefreshJump = 0;
+
     private string jumpButtonInputName = "Jump";
 
     // Cached Component Refences
     private Rigidbody2D myRigidbody2D = null;
+
+    private CapsuleCollider2D myCapsuleCollider2D = null;
 
     private void Start()
     {
         CacheComponentReferences();
 
         myRigidbody2D.gravityScale = gravityMultiplier;
+
+        layerToTouchForRefreshJump = LayerMask.GetMask(layerToTouchForRefreshedJumpString);
     }
 
     private void Update()
+    {
+        ListenForJumpInputs();
+    }
+
+    private void ListenForJumpInputs()
+    {
+        bool jumpButtonPressed = Input.GetButtonDown("Jump");
+
+        Jump(jumpButtonPressed);
+    }
+
+    private void Jump(bool jumpButtonPressed)
+    {
+        ApplyExtraJumpGravity();
+
+        bool ableToJump = myCapsuleCollider2D.IsTouchingLayers(layerToTouchForRefreshJump);
+
+        if (jumpButtonPressed && ableToJump)
+        { 
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+
+            myRigidbody2D.velocity += jumpVelocityToAdd;
+
+            ableToJump = false;
+        }
+    }
+
+    private void ApplyExtraJumpGravity()
     {
         if (myRigidbody2D.velocity.y < 0)
         {
@@ -42,5 +81,6 @@ public class BetterJump : MonoBehaviour
     private void CacheComponentReferences()
     {
         myRigidbody2D = GetComponent<Rigidbody2D>();
+        myCapsuleCollider2D = GetComponent<CapsuleCollider2D>();
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,13 +7,6 @@ public class Player : MonoBehaviour
     // Config
     [SerializeField]
     private float runSpeed = 5.0f;
-    [SerializeField]
-    private float jumpSpeed = 9.0f;
-
-
-    // State
-    bool isAlive = true;
-
 
     // Cached Component References
     private Rigidbody2D myRigidbody2D = null;
@@ -34,23 +27,12 @@ public class Player : MonoBehaviour
     private void ListenForMovementInputs()
     {
         Run(Input.GetAxis("Horizontal"));
-        Jump(Input.GetButtonDown("Jump"));
     }
 
     private void Run(float runButtonPressedAmount)
     {
         Vector2 moveSpeed = new Vector2(runSpeed * runButtonPressedAmount, myRigidbody2D.velocity.y);
         myRigidbody2D.velocity = moveSpeed;
-    }
-
-    private void Jump(bool jumpButtonPressed)
-    {
-        if (jumpButtonPressed)
-        { 
-            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
-
-            myRigidbody2D.velocity += jumpVelocityToAdd;
-        }
     }
 
     private void CacheComponentReferences()
