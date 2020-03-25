ad6078986ebb4903fb2f0022140a38580eacc1d4
datrickpover <48770489+datrickpover@users.noreply.github.com>
2020-03-25 08:52:26 -0400

    jump!

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
     // Config
     [SerializeField]
     private float runSpeed = 5.0f;
+    [SerializeField]
+    private float jumpSpeed = 9.0f;
 
 
     // State
@@ -27,12 +29,23 @@ public class Player : MonoBehaviour
     void Update()
     {
         Run();
+        Jump();
     }
 
     private void Run()
     {
         Vector2 moveSpeed = new Vector2(runSpeed * Input.GetAxis("Horizontal"), myRigidbody2D.velocity.y);
         myRigidbody2D.velocity = moveSpeed;
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        { 
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+
+            myRigidbody2D.velocity += jumpVelocityToAdd;
+        }
     }
 
     private void CacheComponentReferences()
