09540739e037dc1bd6d1fcaca1053bf77680ebc1
datrickpover <48770489+datrickpover@users.noreply.github.com>
2020-03-31 10:36:27 -0400

    scrolling water
    also adjusted player head collider

--- Assets/Scripts/PlayerHead.cs
+++ Assets/Scripts/PlayerHead.cs
@@ -16,7 +16,6 @@ public class PlayerHead : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D otherCollider2D)
     {
-        Debug.Log("head trigger enter");
         if (1 << otherCollider2D.gameObject.layer == waterLayer.value) //https://forum.unity.com/threads/get-the-layernumber-from-a-layermask.114553/
         {
             KillPlayer();
--- Assets/Scripts/VerticalScroll.cs
+++ Assets/Scripts/VerticalScroll.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class VerticalScroll : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float scrollSpeed = 0.2f;
+    private Rigidbody2D myRigidbody2D;
+
+    private void Start()
     {
-        
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+        ScrollUp();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //ScrollUp();
+    }
+
+    private void ScrollUp()
+    {
+        myRigidbody2D.velocity = new Vector2(0f, scrollSpeed);
     }
 }
